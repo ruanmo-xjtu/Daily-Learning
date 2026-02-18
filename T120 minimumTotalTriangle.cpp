93edffc521dcbfccd25704c5a3f37fcf70d2354f
ruanmo-xjtu <2176127693@qq.com>
2026-02-18 18:18:01 +0800

    Add files via upload

---
+++ T120 minimumTotalTriangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> f(n);
+        f[0] = triangle[0][0];
+        for (int i = 1; i < n; ++i) {
+            f[i] = f[i - 1] + triangle[i][i];
+            for (int j = i - 1; j > 0; --j) {
+                f[j] = min(f[j - 1], f[j]) + triangle[i][j];
+            }
+            f[0] += triangle[i][0];
+        }
+        return *min_element(f.begin(), f.end());
+    }
+};
